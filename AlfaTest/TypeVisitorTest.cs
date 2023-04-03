f679b2dc222d4a82c2f05d6759e079cd1b91cce4
Bone <Asbjoernjc@gmail.com>
2023-04-03 16:05:46 +0200

    TypeVisitorTest is no longer duplicate

--- AlfaTest/TypeVisitorTest.cs
+++ AlfaTest/TypeVisitorTest.cs
@@ -4,13 +4,46 @@ namespace AlfaTest;
 
 public class TypeVisitorTest
 {
-    public static string path = "../../../AlfaTestProgForType.json";
-
     [Theory]
-    [ClassData(typeof(TypeTestData))]
-    public void TypeTestThrowException(string prog, string comment)
+    [ClassData(typeof(FalseTypeTestData))]
+    public void TypeTestThrowException(string prog, string comment, Type exceptionType)
     {
-        Assert.ThrowsAny<Exception>(() => Prog.MyParseMethod(prog: prog, output: "../../../../ALFA/Output/sketch.js"));
+        try
+        {
+            Prog.MyParseMethod(prog: prog, output: "../../../../ALFA/Output/sketch.js");
+            Assert.True(false, "Expected exception was not thrown");
+        }
+        catch (Exception actualException)
+        {
+            switch (actualException)
+            {
+                case TypeException:
+                    Assert.Equal(exceptionType, typeof(TypeException));
+                    break;
+                case ArgumentTypeException:
+                    Assert.Equal(exceptionType, typeof(ArgumentTypeException));
+                    break;
+                case NumArgumentException:
+                    Assert.Equal(exceptionType, typeof(NumArgumentException));
+                    break;
+                case UnknownBuiltinException:
+                    Assert.Equal(exceptionType, typeof(UnknownBuiltinException));
+                    break;
+                case UndeclaredVariable:
+                    Assert.Equal(exceptionType, typeof(UndeclaredVariable));
+                    break;
+                default:
+                    Assert.Equal(new Exception("randomstuff"), actualException);
+                    break;
+            }
+        }
+    }
+    [Theory]
+    [ClassData(typeof(TrueTypeTestData))]
+    public void TypeTestPass(string prog, string comment, Type exceptionType)
+    {
+        Prog.MyParseMethod(prog: prog, output: "../../../../ALFA/Output/sketch.js");
+        Assert.True(true);
     }
 }
 
