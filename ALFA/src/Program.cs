077117aaec5810bbfda5420e21c7896b627cb5ab
caspermortensen <cmor21@student.aau.dk>
2023-04-04 10:49:14 +0200

    added a test to typecheck that will have to be fixed later on nullException + comment typecheck vardcl node

--- ALFA/src/Program.cs
+++ ALFA/src/Program.cs
@@ -18,7 +18,11 @@ namespace ALFA
             string _output = output == "" ? "../../../Output/sketch.js" : "../../../../ALFA/Output/sketch.js";
             
             String input = prog == "" ? File.ReadAllText(path) : prog;
-            
+
+            if (prog == "int k = 0;\nint y = 0;\n\nint length = 20;\nint animDuration = 4000;\nint delay = 2000;\nsquare mySquare1 = createSquare(0, 0, length, length);\nint i = mySquare1;\nmove(mySquare1 , 200, animDuration);\nwait(delay);\nmove(mySquare1 , -200, animDuration);")
+            {
+                Console.WriteLine("WTH");
+            }
             ICharStream stream = CharStreams.fromString(input);
             ITokenSource lexer = new ALFALexer(stream);
             ITokenStream tokens = new CommonTokenStream(lexer);
@@ -26,6 +30,8 @@ namespace ALFA
             parser.BuildParseTree = true;
             IParseTree tree = parser.program();
 
+            
+            
             SymbolTable symbolTable = new();
             BuildASTVisitor visitor = new BuildASTVisitor(symbolTable);
             Node ast = visitor.Visit(tree);
