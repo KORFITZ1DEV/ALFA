3e05baae5cd135b36986f585549e988ef2a28d41
AsbjoernJC <asbjoernjc@gmail.com>
2023-04-19 14:37:06 +0200

    Added new folder for parsing test

---
+++ AlfaTest/Parser/ParserTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+namespace AlfaTest.Parser;
+
+public class ParserTest
+{
+
+}
+
+public class ParserTestData : IEnumerable<object[]>
+{
+    public IEnumerator<object[]> GetEnumerator()
+    {
+        yield return new object[] { item.Prog, item.Comment, exceptionType };
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
