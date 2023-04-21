39d9ee0a0a02986a28e85892184561f7e9432fea
AsbjoernJC <asbjoernjc@gmail.com>
2023-04-21 11:29:58 +0200

    added dir for a parsermocker which will mock parse trees

--- AlfaTest/Parser/ParserTest.cs
+++ AlfaTest/Parser/ParserTest.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using Antlr4.Runtime;
 using Antlr4.Runtime.Tree;
+using Moq;
 
 namespace AlfaTest.Parser;
 
 public class ParserTest
 {
     private readonly ALFAParser _sut;
+    
     private readonly Mock<ICharStream> _iCharStreamMock = new Mock<ICharStream>();
     private readonly Mock<ITokenSource> _iTokenSourceMock = new Mock<ITokenSource>();
     private readonly Mock<ITokenStream> _iTokenStreamMock = new Mock<ITokenStream>();
@@ -29,26 +31,13 @@ public class ParserTest
 
 public class ParserTestData : IEnumerable<object[]>
 {
+    private ParserMocker _parserMocker = new ParserMocker();
     public IEnumerator<object[]> GetEnumerator()
     {
-        /* Line 28 program. Children in IParseTree
-        [0] = {ALFAParser.StatementContext} {Antlr4.Runtime.Tree.IRuleNode.RuleContext: {[16]}}
-        [1] = {ALFAParser.StatementContext} {Antlr4.Runtime.Tree.IRuleNode.RuleContext: {[16]}}
-        [2] = {ALFAParser.StatementContext} {Antlr4.Runtime.Tree.IRuleNode.RuleContext: {[16]}}
-        [3] = {ALFAParser.StatementContext} {Antlr4.Runtime.Tree.IRuleNode.RuleContext: {[16]}}
-        [4] = {ALFAParser.StatementContext} {Antlr4.Runtime.Tree.IRuleNode.RuleContext: {[16]}}
-        [5] = {ALFAParser.StatementContext} {Antlr4.Runtime.Tree.IRuleNode.RuleContext: {[16]}}
-        [6] = {ALFAParser.StatementContext} {Antlr4.Runtime.Tree.IRuleNode.RuleContext: {[16]}}
-        [7] = {ALFAParser.StatementContext} {Antlr4.Runtime.Tree.IRuleNode.RuleContext: {[16]}}
-        [8] = {ALFAParser.StatementContext} {Antlr4.Runtime.Tree.IRuleNode.RuleContext: {[16]}}
-        [9] = {ALFAParser.StatementContext} {Antlr4.Runtime.Tree.IRuleNode.RuleContext: {[16]}}
-        [10] = {ALFAParser.StatementContext} {Antlr4.Runtime.Tree.IRuleNode.RuleContext: {[16]}}
-        [11] = {ALFAParser.StatementContext} {Antlr4.Runtime.Tree.IRuleNode.RuleContext: {[16]}}
-        [12] = {ALFAParser.StatementContext} {Antlr4.Runtime.Tree.IRuleNode.RuleContext: {[16]}}
-        [13] = {ALFAParser.StatementContext} {Antlr4.Runtime.Tree.IRuleNode.RuleContext: {[16]}}
-        [14] = {ALFAParser.StatementContext} {Antlr4.Runtime.Tree.IRuleNode.RuleContext: {[16]}}
-        [15] = {ALFAParser.StatementContext} {Antlr4.Runtime.Tree.IRuleNode.RuleContext: {[16]}}
-        [16] = {TerminalNodeImpl} <EOF>*/
+        List<ALFAParser.StatementContext> parseTree = _parserMocker.MockParseTree();
+        
+        yield return new object[] { "var x = 1;" };
+
         
         yield return new object[] {"2 plus 7"};
     }
---
+++ AlfaTest/ParserMocker/ParserMocker.cs
@@ -0,0 +1,63 @@
+﻿using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+
+namespace AlfaTest;
+
+public class ParserMocker
+{
+    private int _myInvokingState = 0;
+    
+    public ParserMocker()
+    {
+        _myInvokingState = 0;
+    }
+
+    public List<ALFAParser.StatementContext> MockParseTree()
+    {
+        //TODO needs to change so one can pass statements to it and it creates the tree based on the statemenmts
+        
+        List<ALFAParser.StatementContext> alfaTree = new List<ALFAParser.StatementContext>();
+        
+        ALFAParser.ProgramContext programContext = new ALFAParser.ProgramContext(null, _myInvokingState++);
+        ALFAParser.StatementContext stmtWithVarDclNode = new ALFAParser.StatementContext(programContext, _myInvokingState++);
+        ALFAParser.TypeContext terminalIntNode = new ALFAParser.TypeContext(stmtWithVarDclNode, _myInvokingState++);
+        ALFAParser.VarDclContext varDclNode = new ALFAParser.VarDclContext(stmtWithVarDclNode, _myInvokingState++);
+        TerminalNodeImpl semiColonImpl = new TerminalNodeImpl(new CommonToken(1, ";"));
+        
+        TerminalNodeImpl terminalNodeImplInt = new TerminalNodeImpl(new CommonToken(9, "int"));
+
+        //Assigning child and parent to TypeContext node
+        terminalIntNode.AddChild(terminalNodeImplInt);
+        terminalNodeImplInt.Parent = terminalIntNode;
+        
+        //Assigning child and parent to StatementContext node
+        stmtWithVarDclNode.AddChild(terminalIntNode);
+        stmtWithVarDclNode.AddChild(varDclNode);
+        stmtWithVarDclNode.AddChild(semiColonImpl);
+        semiColonImpl.Parent = stmtWithVarDclNode;
+        
+        //Assigning child and parent to VarDclContext node
+        TerminalNodeImpl identifierImpl = new TerminalNodeImpl(new CommonToken(11, "x1"));
+        TerminalNodeImpl equalSignImpl = new TerminalNodeImpl(new CommonToken(2, "="));
+        TerminalNodeImpl rightSideAssignmentImpl = new TerminalNodeImpl(new CommonToken(12, "0"));
+        
+        varDclNode.AddChild(identifierImpl);
+        identifierImpl.Parent = varDclNode;
+        
+        varDclNode.AddChild(equalSignImpl);
+        equalSignImpl.Parent = varDclNode;
+        
+        varDclNode.AddChild(rightSideAssignmentImpl);
+        rightSideAssignmentImpl.Parent = varDclNode;
+        
+        return alfaTree;
+    }
+
+    public List<ALFAParser.StatementContext> MockParseTree(string input)
+    {
+        //should tokenize input to create a tree. 
+        
+        return new List<ALFAParser.StatementContext>();
+    }
+
+}
