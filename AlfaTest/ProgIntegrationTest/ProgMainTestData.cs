b231f2dab9fe31cac04566b9d05af2fc2d271d6f
Bone <Asbjoernjc@gmail.com>
2023-05-17 07:20:16 +0200

    Disabled test parallelization and moved some integration tests in typechecking to ProgIntegrationTest

---
+++ AlfaTest/ProgIntegrationTest/ProgMainTestData.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace AlfaTest.ProgTest;
+
+
+public class ProgMainNoExceptionTestData : JsonTestData
+{
+    private string _path = "../../../ProgIntegrationTest/ProgMainNoException.json";
+
+    public override Item[] LoadJson()
+    {
+        return JsonConvert.DeserializeObject<Item[]>(File.ReadAllText(_path));
+    }
+}
---
+++ AlfaTest/ProgIntegrationTest/ProgMainThrowsExceptionTestData.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace AlfaTest.ProgTest;
+
+
+public class ProgMainThrowsExceptionTestData : JsonTestData
+{
+    private string _path = "../../../ProgIntegrationTest/ProgMainThrowsException.json";
+    public override Item[] LoadJson()
+    {
+        return JsonConvert.DeserializeObject<Item[]>(File.ReadAllText(_path));
+    }    
+}
