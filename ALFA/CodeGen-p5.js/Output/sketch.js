d1c6c37f6a5e69064fee415ee18a457ba286640f
Bone <Asbjoernjc@gmail.com>
2023-06-22 07:39:01 +0200

    Tested all progmainnoexception programs with my eyes

---
+++ ALFA/CodeGen-p5.js/Output/sketch.js
@@ -0,0 +1,91 @@
+const shapesToDraw = [];
+
+class Shape {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        shapesToDraw.push(this);
+    }
+
+    move(offsetX, offsetY, duration) {
+        let start = Date.now();
+
+        let originalX = this.x;
+        let originalY = this.y;
+
+        let animate = (resolve) => {
+            let now = Date.now();
+            let progress = (now - start) / duration;
+
+            if (progress >= 1) {
+                this.x = originalX + offsetX;
+                this.y = originalY + offsetY;
+                resolve(); // Resolving the Promise here
+                return;
+            }
+
+            let dx = progress * offsetX;
+            let dy = progress * offsetY;
+
+            this.x = originalX + dx;
+            this.y = originalY + dy;
+
+            requestAnimationFrame(() => animate(resolve)); // Passing `resolve` to the next frame
+        }
+
+        return new Promise((resolve, reject) => {
+            animate(resolve); // Passing `resolve` to the `animate` function
+        });
+    }
+}
+
+class Rect extends Shape {
+    constructor(x, y, w, h) {
+        super(x, y);
+        this.w = w;
+        this.h = h;
+    }
+
+    draw() {
+        rect(this.x, this.y, this.w, this.h);
+    }
+}
+
+function wait(duration) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+        }, duration)
+    });
+}
+
+async function moveParal(animations) {
+    const promises = animations.map(animation => animation());
+    return Promise.all(promises);
+}
+
+
+async function main() {
+	let var_myRect=new Rect(300,300,100,100)
+	let var_myRect2=var_myRect
+	let var_myRect3=var_myRect2
+
+	await moveParal([		
+		() => var_myRect.move(100,0,600),
+		() => var_myRect2.move(100,100,600),	
+	]);
+
+}
+
+function setup() {
+	createCanvas(1000, 1000)
+	main();
+}
+
+function draw() {
+	background(255)
+
+	for (const shape of shapesToDraw) {
+		shape.draw()
+	}
+}
